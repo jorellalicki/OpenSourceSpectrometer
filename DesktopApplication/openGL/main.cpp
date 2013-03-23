0f1b3301d6da7b9cbccdb32e3e267a3734b1f214
Justin Jones <jsjones27@gmail.com>
2013-03-23 15:05:35 -0400

    further organized opengl app

--- DesktopApplication/openGL/main.cpp
+++ DesktopApplication/openGL/main.cpp
@@ -1,3 +1,9 @@
+/*
+	TODO:
+	Implemnt appropriate data-polling
+	This should probably happen in display function, after flushing
+*/
+
 #include <GL/glut.h>
 #include <iostream>
 
@@ -6,6 +12,11 @@ using namespace std;
 #define WINDOW_INIT_WIDTH 1000
 #define WINDOW_INIT_HEIGHT 800
 
+// This can be used to do stuff while idling
+void onIdle(void) {
+}
+
+// OpenGL display routine
 void drawScene(void)
 {
 	cout << "drawScene" << endl;
@@ -17,11 +28,17 @@ void drawScene(void)
 	glLoadIdentity();
 	
 	glBegin(GL_QUADS);
-	glVertex2f(x1, y1); glVertex2f(x2, y1); glVertex2f(x2, y2); glVertex2f(x1, y2);
+	glVertex2f(x1, y1); glVertex2f(2*x2, y1); glVertex2f(x2, y2); glVertex2f(x1, y2);
 	glEnd();
 	
-	glFlush();
-//	SwapBuffers();
+	// Use double-buffering to prevent tearing
+	glutSwapBuffers();
+	
+	// Force re-display (flush)
+	glutPostRedisplay();
+	
+	
+//	glFlush(); // could possibly use this instead of glutpostredisplay?
 }
 
 // OpenGL window reshape/resize routine.
@@ -36,7 +53,7 @@ void reshape(int w, int h)
 	
 	glMatrixMode (GL_PROJECTION);
 	glLoadIdentity ();
-	glOrtho (0, w, h, 0, 0, 1);
+	glOrtho (0, 1000, 800, 0, 0, 1);
 	glMatrixMode (GL_MODELVIEW);
 }
 
@@ -51,6 +68,13 @@ void setup(void)
 	
 	// Disable depth-testing because we are using 2D
 	glDisable(GL_DEPTH_TEST);
+	
+	/*
+	TODO: figure out glXSwapIntervalEXT (or some other fucntion) to enable v-sync/frame-limiting
+	Maybe just use timers in display function?
+	*/
+		
+//	glXSwapIntervalEXT(1);
 }
 
 
@@ -73,6 +97,7 @@ void keyInput(unsigned char key, int x, int y)
 		case 'X':
 			cout << "you pressed X" << endl;
 			break;
+			
 		default:
 			break;
 	}
@@ -84,14 +109,18 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(WINDOW_INIT_WIDTH, WINDOW_INIT_HEIGHT);
 	glutInitWindowPosition(100, 100); 
 	glutCreateWindow("Spectrometer Graph");
+
 	setup(); 
 	glutDisplayFunc(drawScene); 
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyInput);
-	glutMainLoop(); 
+	glutIdleFunc(onIdle);
+	
+	
+	glutMainLoop(); // this blocks; nothing past here is executed
 
 	return 0;
 }
